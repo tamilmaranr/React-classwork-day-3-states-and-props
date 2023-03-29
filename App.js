4a511038b680788a74330ee0e064880e0e03c1b6
tamilmaranr <108916935+tamilmaranr@users.noreply.github.com>
2023-03-29 15:14:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Naving from './Naving'
+
+
+
+
+function App() {
+  return (
+		<div className="App">
+      <center>
+      <Naving/>
+      </center>
+	</div>
+  )
+}
+
+export default App
